23bfafd5fe9bda03ca6697de619e5ccbb408e6eb
s-techlead <sebastienschnd.techlead@gmail.com>
2023-07-15 20:47:28 +0200

    Starting auth page component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type NavbarProps = {
+    
+};
+
+const Navbar:React.FC<NavbarProps> = () => {
+    
+    return <div>Narbar</div>
+}
+export default Navbar;
---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,16 @@
+import Navbar from '@/components/Navbar/Navbar';
+import React from 'react';
+
+type AuthPageProps = {
+    
+};
+
+const index:React.FC<AuthPageProps> = () => {
+    
+    return <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
+        <div className="max-w-7xl mx-auto">
+            <Navbar />
+        </div>
+    </div>
+}
+export default index;
