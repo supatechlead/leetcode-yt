40e022dcbbe2d51ab3ada94a8d0e387f5398d1a5
s-techlead <sebastienschnd.techlead@gmail.com>
2023-07-22 17:17:04 +0200

    Recoil state implementation and sign in click button handling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,16 @@
+import { authModalState } from "@/atoms/authModalAtom";
 import Link from "next/link"
 import React from 'react';
-
+import { useSetRecoilState } from "recoil";
 type NavbarProps = {
     
 };
 
 const Navbar:React.FC<NavbarProps> = () => {
-    
+    const setAuthModalState = useSetRecoilState(authModalState)
+    const handleClick = () => {
+        setAuthModalState((prev) => ({ ...prev, isOpen: true }))
+    };
     return <div className='flex items-center justify-between sm:px-12 px-2 md:px-24'>
         <Link href="/" className="flex items-center justify-center h-20">
             <img src="/logo.png" alt="LeetClone" className="h-full" />
@@ -17,6 +21,7 @@ const Navbar:React.FC<NavbarProps> = () => {
                 hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange border-2 border-transparent
                 transition-duration-300 ease-in-out
                 " 
+                onClick={handleClick}
             >Sign In</button>
         </div>
 
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,21 +1,20 @@
+import { authModalState } from '@/atoms/authModalAtom';
 import AuthModal from '@/components/Modals/AuthModal';
 import Navbar from '@/components/Navbar/Navbar';
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+type AuthPageProps = {};
 
-type AuthPageProps = {
-    
-};
-
-const index:React.FC<AuthPageProps> = () => {
-    
+const AuthPage:React.FC<AuthPageProps> = () => {
+    const authModal = useRecoilValue(authModalState)
     return <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
         <div className="max-w-7xl mx-auto">
             <Navbar />
             <div className='flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none'>
                 <img src="/hero.png" alt="Hero img" />
             </div>
-            <AuthModal />
+            {authModal.isOpen && <AuthModal />}
         </div>
     </div>
 }
-export default index;
+export default AuthPage;
