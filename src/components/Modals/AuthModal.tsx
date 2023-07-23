4536fc4133c57b60e5337c5d310f0c2d800da0f3
s-techlead <sebastienschnd.techlead@gmail.com>
2023-07-23 11:55:48 +0200

    Ending Authmodal handling

--- src/components/Modals/AuthModal.tsx
+++ src/components/Modals/AuthModal.tsx
@@ -1,7 +1,7 @@
 import { authModalState } from '@/atoms/authModalAtom';
-import React from 'react';
+import React, { useEffect } from "react";
 import { IoClose } from "react-icons/io5"
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import Login from "./Login"
 import ResetPassword from './ResetPassword';
 import Signup from './Signup';
@@ -9,17 +9,20 @@ import Signup from './Signup';
 type AuthModalProps = {};
 
 const AuthModal:React.FC<AuthModalProps> = () => {
-    const authModal = useRecoilValue(authModalState)
+    const authModal = useRecoilValue(authModalState);
+    const closeModal = useCloseModal()
     return (
         <>
-            <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60'></div>
+            <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60' onClick={closeModal}></div>
             <div className='w-full sm:w-[450px]  absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  flex justify-center items-center'>
                 <div className='relative w-full h-full mx-auto flex items-center justify-center'>
                     <div className='bg-white rounded-lg shadow relative w-full bg-gradient-to-b from-brand-orange to-slate-900 mx-6'>
                         <div className='flex justify-end p-2'>
                             <button
                                 type='button'
-                                className='bg-transparent  rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 hover:text-white text-white'
+                                className='bg-transparent  rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 
+                                hover:text-white text-white'
+                                onClick={closeModal}
                             >
                                 <IoClose className='h-5 w-5'/>
                             </button>
@@ -32,3 +35,21 @@ const AuthModal:React.FC<AuthModalProps> = () => {
     )
 }
 export default AuthModal;
+
+function useCloseModal() {
+	const setAuthModal = useSetRecoilState(authModalState);
+
+	const closeModal = () => {
+		setAuthModal((prev) => ({ ...prev, isOpen: false }));
+	};
+
+	useEffect(() => {
+		const handleEsc = (e: KeyboardEvent) => {
+			if (e.key === "Escape") closeModal();
+		};
+		window.addEventListener("keydown", handleEsc);
+		return () => window.removeEventListener("keydown", handleEsc);
+	}, []);
+
+	return closeModal;
+}
