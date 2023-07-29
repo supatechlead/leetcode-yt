56d5a27200e493cbbaf3aea4243e065669ed9b35
s-techlead <sebastienschnd.techlead@gmail.com>
2023-07-29 16:30:20 +0200

    Login functionality and auth page route guard completion

--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -1,22 +1,57 @@
 import { authModalState } from '@/atoms/authModalAtom';
-import React from 'react';
+import { auth } from "@/firebase/firebase";
+import { useRouter } from "next/router";
+import Router from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useSetRecoilState } from 'recoil';
+import { ImportsNotUsedAsValues } from 'typescript';
 
 type LoginProps = {};
 
 const Login:React.FC<LoginProps> = () => {
+    
     const setAuthModalState = useSetRecoilState(authModalState)
+    
     const handleClick = (type:"login" | "register" | "forgotPassword") => {
         setAuthModalState((prev) => ({ ...prev, type }))
     };
+    
+    const [inputs, setInputs ] = useState({ email: "", password: ""});
+    
+    const [signInWithEmailAndPasword, user, loading, error] = useSignInWithEmailAndPassword(auth)
+
+    const router = useRouter();
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputs((prev) => ({...prev, [e.target.name]: e.target.value}));
+    }
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if(!inputs.email || !!inputs.password) return alert("Please fill all fields");
+        try {
+            const newuser = await signInWithEmailAndPasword(inputs.email, inputs.password)
+            if (!newuser) return;
+            router.push("/")
+        } catch(error:any) {
+            alert(error.message);
+        }
+    }
+    
+    useEffect(() => {
+        if (error) alert(error.message);
+    }, [error])
+
     return (
-        <form className='space-y-6 px-6 pb-4'>
+        <form className='space-y-6 px-6 pb-4' onSubmit={handleLogin}>
             <h3 className='text-xl font-medium text-white'>Sign in to LeetClone</h3>
             <div>
                 <label htmlFor="email" className='text-sm font-medium block mb-2 text-gray-300'>
                 Your Email
                 </label>
                 <input 
+                    onChange={handleInputChange}
                     type="email" 
                     name="email" 
                     id="email" 
@@ -32,6 +67,7 @@ const Login:React.FC<LoginProps> = () => {
                 Your Email
                 </label>
                 <input 
+                    onChange={handleInputChange}
                     type="password" 
                     name="password" 
                     id="password" 
@@ -47,7 +83,7 @@ const Login:React.FC<LoginProps> = () => {
                 className="w-full text-white focus:ring-blue-300 font-medium rounded-lg
                     text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s"   
             >
-                Login
+                {loading ? "Loading..." : "Log In"}
             </button>
 
             <button className='flex w-full justify-end' onClick={() => handleClick("forgotPassword")}>
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,12 +1,27 @@
 import { authModalState } from '@/atoms/authModalAtom';
 import AuthModal from '@/components/Modals/AuthModal';
 import Navbar from '@/components/Navbar/Navbar';
-import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from "@/firebase/firebase"
 import { useRecoilValue } from 'recoil';
+import { useState, useEffect } from 'react';
+import Router from 'next/router';
+import { useRouter } from "next/router"
 type AuthPageProps = {};
 
 const AuthPage:React.FC<AuthPageProps> = () => {
     const authModal = useRecoilValue(authModalState)
+    const [user, loading, error] = useAuthState(auth)
+    const [pageLoading, setPageLoading] = useState(true)
+    const router = useRouter();
+
+    useEffect(() => {
+        if(user) router.push('/');
+        if(!loading && !user) setPageLoading(false);
+    },[user, router, loading]);
+
+    if(pageLoading) return null;
+
     return <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
         <div className="max-w-7xl mx-auto">
             <Navbar />
