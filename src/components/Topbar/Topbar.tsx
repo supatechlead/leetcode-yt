e2dc1aefd91bb2668e2c0012d4f5f0deb181397a
s-techlead <sebastienschnd.techlead@gmail.com>
2023-07-30 16:09:21 +0200

    Topbar update completion

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -35,7 +35,14 @@ const Topbar:React.FC<TopbarProps> = () => {
 					)}
 					{user && (
 						<div className="cursor-pointer group relative">
-
+							<img src="/avatar.png" alt="user profile img" className="h-8 w_8 rounded-full" />
+							<div
+								className='absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
+								z-40 group-hover:scale-100 scale-0 
+								transition-all duration-300 ease-in-out'
+							>
+								<p className='text-sm'>{user.email}</p>
+							</div>
 						</div>
 					)}
 				</div>
