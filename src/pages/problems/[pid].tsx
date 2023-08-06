ce3cba591e8b40f392941f6c25d63b2a75617ce7
s-techlead <sebastienschnd.techlead@gmail.com>
2023-08-06 18:36:12 +0200

    Adding problems page

---
+++ src/pages/problems/[pid].tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type ProblemPageProps = {
+    
+};
+
+const ProblemPage:React.FC<ProblemPageProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default ProblemPage;
