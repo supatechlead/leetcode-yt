e9eafd192576990ec5391a69df3d99cd26ee646e
s-techlead <sebastienschnd.techlead@gmail.com>
2023-08-07 14:51:03 +0200

    Adding playground and preference nav bar

---
+++ src/components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type PreferenceNavProps = {
+    
+};
+
+const PreferenceNav:React.FC<PreferenceNavProps> = () => {
+    
+    return (
+        <div>
+            PreferenceNav
+        </div>
+    )
+}
+export default PreferenceNav;
