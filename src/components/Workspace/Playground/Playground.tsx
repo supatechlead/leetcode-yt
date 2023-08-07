1d11499f15d509fb377a2337e257333571335dd0
s-techlead <sebastienschnd.techlead@gmail.com>
2023-08-07 16:32:09 +0200

    Adding codemirror editor

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import PreferenceNav from './PreferenceNav/PreferenceNav';
+import Split from "react-split";
+import CodeMirror from "@uiw/react-codemirror";
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+import { javascript } from "@codemirror/lang-javascript";
 
 type PlaygroundProps = {
     
@@ -8,9 +12,22 @@ type PlaygroundProps = {
 const Playground:React.FC<PlaygroundProps> = () => {
     
     return (
-        <>
+        <div className="flex flex-col bg-dark-layer-1 relative">
           <PreferenceNav />  
-        </>
+          <Split className='h-[calc(100vh-94px)]' direction='vertical' sizes={[60, 40]} minSize={60}>
+            <div className='w-full overflow-auto'>
+            <CodeMirror
+              value="const a = 1;"
+						  theme={vscodeDark}
+						  extensions={[javascript()]}
+						  style={{ fontSize: 16 }}
+					/>
+            </div>
+            <div>
+              tescases
+            </div>
+          </Split>
+        </div>
     )
 }
 export default Playground;
