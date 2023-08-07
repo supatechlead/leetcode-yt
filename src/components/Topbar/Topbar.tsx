5c944eaeadd0cd2faaa1fde8d43237302f76ac44
s-techlead <sebastienschnd.techlead@gmail.com>
2023-08-07 13:56:16 +0200

    Adding timer

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -8,15 +8,17 @@ import Logout from "../Buttons/Logout";
 import { authModalState } from "@/atoms/authModalAtom";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { BsList } from "react-icons/bs";
+import Timer from "../Timer/Timer";
 
 type TopbarProps = {
-	problemPage: boolean;
+	problemPage?: boolean;
 };
 
 const Topbar:React.FC<TopbarProps> = ({ problemPage}) => {
 	const [user] = useAuthState(auth);
-	const setAuthModalState = useSetRecoilState(authModalState)
-    return (
+	const setAuthModalState = useSetRecoilState(authModalState);
+    
+	return (
         <nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
             <div className={`flex w-full items-center justify-between ${!problemPage ? "max-w-[1200px] mx-auto" : ""} `}>
                 <Link href='/' className='h-[22px] flex-1'>
@@ -59,9 +61,10 @@ const Topbar:React.FC<TopbarProps> = ({ problemPage}) => {
 							<button className='bg-dark-fill-3 py-1 px-2 cursor-pointer rounded '>Sign In</button>
 						</Link>
 					)}
+					{user && problemPage && <Timer />}
 					{user && (
 						<div className="cursor-pointer group relative">
-							<img src="/avatar.png" alt="user profile img" className="h-8 w_8 rounded-full" />
+							<Image src="/avatar.png" alt="Avatar" width={30} height={30} className="rounded-full" />
 							<div
 								className='absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
 								z-40 group-hover:scale-100 scale-0 
