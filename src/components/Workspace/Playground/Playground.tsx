322ee3625b42ab1f231023cd3483e0f0018417c3
s-techlead <sebastienschnd.techlead@gmail.com>
2023-08-08 11:14:00 +0200

    Adding test case bullets

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -23,8 +23,49 @@ const Playground:React.FC<PlaygroundProps> = () => {
 						  style={{ fontSize: 16 }}
 					/>
             </div>
-            <div>
-              tescases
+            <div className='w-full px-5 overflow-auto'>
+              {/* test case heading */}
+              <div className='flex h-10 items-center space-x-6'>
+                <div className='relative flex h-full flex-col justify-center cursor-pointer'>
+                  <div className='text-sm font-medium leading-5 text-white'>Testcases</div>
+                  <hr className='absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white' />
+                </div>
+              </div>
+
+              <div className='flex'>
+                
+                {/* case 1 */}
+                <div className='mr-2 items-start mt-2 text-white'>
+                  <div className='flex flex-wrap items-center gap-y-4'>
+                    <div className='font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3
+                      hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>
+                      Case 1
+                    </div>
+                  </div>
+                </div>
+
+                {/* case 2 */}
+                <div className='mr-2 items-start mt-2 text-white'>
+                  <div className='flex flex-wrap items-center gap-y-4'>
+                    <div className='font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3
+                      hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>
+                      Case 2
+                    </div>
+                  </div>
+                </div>
+
+                {/* case 3 */}
+                <div className='mr-2 items-start mt-2 text-white'>
+                  <div className='flex flex-wrap items-center gap-y-4'>
+                    <div className='font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3
+                      hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>
+                      Case 3
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
             </div>
           </Split>
         </div>
