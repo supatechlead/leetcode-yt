3ba3fde2e88dea91769adcd00081abe26f58d4fe
s-techlead <sebastienschnd.techlead@gmail.com>
2023-08-15 16:51:59 +0200

    Fetching data problems examples from local structure

--- src/components/Workspace/ProblemDescription/ProblemDescription.tsx
+++ src/components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -53,42 +53,25 @@ const ProblemDescription: React.FC<ProblemDescriptionProps> = ({ problem }) => {
 
 						{/* Examples */}
 						<div className='mt-4'>
-							{/* Example 1 */}
-							<div>
-								<p className='font-medium text-white '>Example 1: </p>
-								<div className='example-card'>
-									<pre>
-										<strong className='text-white'>Input: </strong> nums = [2,7,11,15], target = 9{" "}
-										<br />
-										<strong>Output:</strong> [0,1] <br />
-										<strong>Explanation:</strong>Because nums[0] + nums[1] == 9, we return [0, 1].
-									</pre>
+						{problem.examples.map((example, index) => (
+								<div key={example.id}>
+									<p className='font-medium text-white '>Example {index + 1}: </p>
+									{example.img && <img src={example.img} alt='' className='mt-3' />}
+									<div className='example-card'>
+										<pre>
+											<strong className='text-white'>Input: </strong> {example.inputText}
+											<br />
+											<strong>Output:</strong>
+											{example.outputText} <br />
+											{example.explanation && (
+												<>
+													<strong>Explanation:</strong> {example.explanation}
+												</>
+											)}
+										</pre>
+									</div>
 								</div>
-							</div>
-
-							{/* Example 2 */}
-							<div>
-								<p className='font-medium text-white '>Example 2: </p>
-								<div className='example-card'>
-									<pre>
-										<strong className='text-white'>Input: </strong> nums = [3,2,4], target = 6{" "}
-										<br />
-										<strong>Output:</strong> [1,2] <br />
-										<strong>Explanation:</strong>Because nums[1] + nums[2] == 6, we return [1, 2].
-									</pre>
-								</div>
-							</div>
-							{/* Example 3 */}
-							<div>
-								<p className='font-medium text-white '>Example 3: </p>
-								<div className='example-card'>
-									<pre>
-										<strong className='text-white'>Input: </strong> nums = [3,3], target = 6
-										<br />
-										<strong>Output:</strong> [0,1] <br />
-									</pre>
-								</div>
-							</div>
+							))}
 						</div>
 
 						{/* Constraints */}
