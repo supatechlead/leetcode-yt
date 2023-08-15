ffb9d7867c03548548068feea0ebe17310aa9779
s-techlead <sebastienschnd.techlead@gmail.com>
2023-08-15 16:32:46 +0200

    Fetching data problems from local structure

--- src/pages/problems/[pid].tsx
+++ src/pages/problems/[pid].tsx
@@ -1,16 +1,52 @@
 import Topbar from "@/components/Topbar/Topbar";
 import Workspace from "@/components/Workspace/Workspace";
+import { problems } from "@/utils/problems";
+import { Problem } from "@/utils/types/problem";
 import React from 'react';
 
-type ProblemPageProps = {};
+type ProblemPageProps = {
+    problem: Problem;
+};
 
-const ProblemPage:React.FC<ProblemPageProps> = () => {
+const ProblemPage:React.FC<ProblemPageProps> = ({ problem }) => {
     
     return (
         <div>
             <Topbar problemPage />
-            <Workspace />
+            <Workspace problem={problem}/>
         </div>
     )
 }
 export default ProblemPage;
+
+// fetch the local data from the API
+// SSG - Static Site Generation
+// getStaticPaths -> it creates the dynamic routes
+export async function getStaticPaths() {
+	const paths = Object.keys(problems).map((key) => ({
+		params: { pid: key },
+	}));
+
+	return {
+		paths,
+		fallback: false,
+	};
+}
+
+// getStaticProps => it fetch the data
+export async function getStaticProps({ params }: { params: { pid: string } }) {
+	const { pid } = params;
+	const problem = problems[pid];
+
+	if (!problem) {
+		return {
+			notFound: true,
+		};
+	}
+	problem.handlerFunction = problem.handlerFunction.toString();
+	return {
+		props: {
+			problem,
+		},
+	};
+}
