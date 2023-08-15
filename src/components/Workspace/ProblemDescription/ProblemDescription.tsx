2b1da4a019948ff18b1d2d00df08c0b1936b6259
s-techlead <sebastienschnd.techlead@gmail.com>
2023-08-15 16:58:59 +0200

    Fetching data problems constraints from local structure

--- src/components/Workspace/ProblemDescription/ProblemDescription.tsx
+++ src/components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -76,21 +76,9 @@ const ProblemDescription: React.FC<ProblemDescriptionProps> = ({ problem }) => {
 
 						{/* Constraints */}
 						<div className='my-5'>
-							<div className='text-white text-sm font-medium'>Constraints:</div>
-							<ul className='text-white ml-5 list-disc'>
-								<li className='mt-2'>
-									<code>2 ≤ nums.length ≤ 10</code>
-								</li>
-
-								<li className='mt-2'>
-									<code>-10 ≤ nums[i] ≤ 10</code>
-								</li>
-								<li className='mt-2'>
-									<code>-10 ≤ target ≤ 10</code>
-								</li>
-								<li className='mt-2 text-sm'>
-									<strong>Only one valid answer exists.</strong>
-								</li>
+						<div className='text-white text-sm font-medium'>Constraints:</div>
+							<ul className='text-white ml-5 list-disc '>
+								<div dangerouslySetInnerHTML={{ __html: problem.constraints }} />
 							</ul>
 						</div>
 					</div>
