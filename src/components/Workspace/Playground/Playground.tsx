2d77bcaaa610f4946aa605e426592d8af8a970d9
s-techlead <sebastienschnd.techlead@gmail.com>
2023-08-16 11:53:01 +0200

    Fetching test cases from local

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState} from 'react';
 import PreferenceNav from './PreferenceNav/PreferenceNav';
 import Split from "react-split";
 import CodeMirror from "@uiw/react-codemirror";
@@ -12,7 +12,7 @@ type PlaygroundProps = {
 };
 
 const Playground:React.FC<PlaygroundProps> = ({problem}) => {
-  
+    const [activeTestCaseId, setActiveTestCaseId] = useState<number>(0);
     return (
         <div className="flex flex-col bg-dark-layer-1 relative overflow-x-hidden">
           <PreferenceNav />  
@@ -35,47 +35,34 @@ const Playground:React.FC<PlaygroundProps> = ({problem}) => {
               </div>
 
               <div className='flex'>
-                
-                {/* case 1 */}
-                <div className='mr-2 items-start mt-2 text-white'>
-                  <div className='flex flex-wrap items-center gap-y-4'>
-                    <div className='font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3
-                      hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>
-                      Case 1
-                    </div>
-                  </div>
-                </div>
-
-                {/* case 2 */}
-                <div className='mr-2 items-start mt-2 text-white'>
-                  <div className='flex flex-wrap items-center gap-y-4'>
-                    <div className='font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3
-                      hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>
-                      Case 2
-                    </div>
-                  </div>
-                </div>
-
-                {/* case 3 */}
-                <div className='mr-2 items-start mt-2 text-white'>
-                  <div className='flex flex-wrap items-center gap-y-4'>
-                    <div className='font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3
-                      hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>
-                      Case 3
-                    </div>
-                  </div>
-                </div>
+              {problem.examples.map((example, index) => (
+							<div
+								className='mr-2 items-start mt-2 '
+								key={example.id}
+                onClick={() => setActiveTestCaseId(index)}
+							>
+								<div className='flex flex-wrap items-center gap-y-4'>
+									<div
+										className={`font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap
+									`}
+									>
+										Case {index + 1}
+									</div>
+								</div>
+							</div>
+						))}
+              
               </div>
 
             {/* test case body */}
             <div className='font-semibold my-4'>
               <p className='text-sm font-medium mt-4 text-white'>Input:</p>
               <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
-                nums: [2,7,11,15], target: 9
+                {problem.examples[activeTestCaseId].inputText}
               </div>
               <p className='text-sm font-medium mt-4 text-white'>Output:</p>
               <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
-                [0, 1]
+                {problem.examples[activeTestCaseId].outputText}
               </div>
             </div>
             </div>
