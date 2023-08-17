32f227ad5d01bd6e4d222a370f05217cd750d362
s-techlead <sebastienschnd.techlead@gmail.com>
2023-08-17 17:20:10 +0200

    Loading skeltons problems

--- src/components/Workspace/ProblemDescription/ProblemDescription.tsx
+++ src/components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -1,5 +1,5 @@
-// import CircleSkeleton from "@/components/Skeletons/CircleSkeleton";
-// import RectangleSkeleton from "@/components/Skeletons/RectangleSkeleton";
+import CircleSkeleton from "@/components/Skeletons/CircleSkeleton";
+import RectangleSkeleton from "@/components/Skeletons/RectangleSkeleton";
 import { auth, firestore } from "@/firebase/firebase";
 import { DBProblem, Problem } from "@/utils/types/problem";
 import { arrayRemove, arrayUnion, doc, getDoc, runTransaction, updateDoc } from "firebase/firestore";
@@ -54,6 +54,16 @@ const ProblemDescription: React.FC<ProblemDescriptionProps> = ({ problem }) => {
 								<div className='cursor-pointer hover:bg-dark-fill-3  rounded p-[3px]  ml-4 text-xl transition-colors duration-200 text-green-s text-dark-gray-6 '>
 									<TiStarOutline />
 								</div>
+							</div>
+						)}
+
+						{loading && (
+							<div className="mt-3 flex space-x-2">
+								<RectangleSkeleton />
+								<CircleSkeleton />
+								<RectangleSkeleton />
+								<RectangleSkeleton />
+								<CircleSkeleton />
 							</div>
 						)}
 
