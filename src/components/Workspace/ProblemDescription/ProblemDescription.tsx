acc374e5101124c3bd41d36e507f79dd860b65fb
s-techlead <sebastienschnd.techlead@gmail.com>
2023-08-18 18:32:01 +0200

    Get user data on the problem

--- src/components/Workspace/ProblemDescription/ProblemDescription.tsx
+++ src/components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -16,7 +16,8 @@ type ProblemDescriptionProps = {
 
 const ProblemDescription: React.FC<ProblemDescriptionProps> = ({ problem }) => {
 	const { currentProblem, loading, problemDifficultyClass, setCurrentProblem } = useGetCurrentProblem(problem.id);
-
+	const { liked, disliked, solved, setData, starred } = useGetUsersDataOnProblem(problem.id);
+	
 	return (
 		<div className='bg-dark-layer-1'>
 			{/* TAB */}
@@ -139,4 +140,32 @@ function useGetCurrentProblem(problemId:string){
 	}, [problemId]);
 
 	return { currentProblem, loading, problemDifficultyClass, setCurrentProblem };
+}
+
+
+function useGetUsersDataOnProblem(problemId: string) {
+	const [data, setData] = useState({ liked: false, disliked: false, starred: false, solved: false });
+	const [user] = useAuthState(auth);
+
+	useEffect(() => {
+		const getUsersDataOnProblem = async () => {
+			const userRef = doc(firestore, "users", user!.uid);
+			const userSnap = await getDoc(userRef);
+			if (userSnap.exists()) {
+				const data = userSnap.data();
+				const { solvedProblems, likedProblems, dislikedProblems, starredProblems } = data;
+				setData({
+					liked: likedProblems.includes(problemId), // likedProblems["two-sum","jump-game"]
+					disliked: dislikedProblems.includes(problemId),
+					starred: starredProblems.includes(problemId),
+					solved: solvedProblems.includes(problemId),
+				});
+			}
+		};
+
+		if (user) getUsersDataOnProblem();
+		return () => setData({ liked: false, disliked: false, starred: false, solved: false });
+	}, [problemId, user]);
+
+	return { ...data, setData };
 }
