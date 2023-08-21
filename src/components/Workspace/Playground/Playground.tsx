2251960ff827ef4e9f4a3942da98984ef96f36a3
s-techlead <sebastienschnd.techlead@gmail.com>
2023-08-21 22:15:11 +0200

    Adding handleSubmit function

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -6,6 +6,12 @@ import { vscodeDark } from "@uiw/codemirror-theme-vscode";
 import { javascript } from "@codemirror/lang-javascript";
 import EditorFooter from './EditorFooter';
 import { Problem } from '@/utils/types/problem';
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, firestore } from "@/firebase/firebase";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { problems } from "@/utils/problems";
 
 type PlaygroundProps = {
 	problem: Problem;
@@ -15,6 +21,67 @@ type PlaygroundProps = {
 
 const Playground:React.FC<PlaygroundProps> = ({ problem, setSuccess, setSolved }) => {
     const [activeTestCaseId, setActiveTestCaseId] = useState<number>(0);
+    let [userCode, setUserCode] = useState<string>(problem.starterCode);
+
+    const [user] = useAuthState(auth);
+	const {
+		query: { pid },
+	} = useRouter();
+
+    const handleSubmit = async () => {
+      if (!user) {
+        toast.error("Please login to submit your code", {
+          position: "top-center",
+          autoClose: 3000,
+          theme: "dark",
+        });
+        return;
+      }
+      try {
+        userCode = userCode.slice(userCode.indexOf(problem.starterFunctionName));
+        const cb = new Function(`return ${userCode}`)();
+        const handler = problems[pid as string].handlerFunction;
+  
+        if (typeof handler === "function") {
+          const success = handler(cb);
+          if (success) {
+            toast.success("Congrats! All tests passed!", {
+              position: "top-center",
+              autoClose: 3000,
+              theme: "dark",
+            });
+            setSuccess(true);
+            setTimeout(() => {
+              setSuccess(false);
+            }, 4000);
+  
+            const userRef = doc(firestore, "users", user.uid);
+            await updateDoc(userRef, {
+              solvedProblems: arrayUnion(pid),
+            });
+            setSolved(true);
+          }
+        }
+      } catch (error: any) {
+        console.log(error.message);
+        if (
+          error.message.startsWith("AssertionError [ERR_ASSERTION]: Expected values to be strictly deep-equal:")
+        ) {
+          toast.error("Oops! One or more test cases failed", {
+            position: "top-center",
+            autoClose: 3000,
+            theme: "dark",
+          });
+        } else {
+          toast.error(error.message, {
+            position: "top-center",
+            autoClose: 3000,
+            theme: "dark",
+          });
+        }
+      }
+    };
+
     return (
         <div className="flex flex-col bg-dark-layer-1 relative overflow-x-hidden">
           <PreferenceNav />  
